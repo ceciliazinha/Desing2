68f255ef758e62b3b83e468515290b4c149dd08f
ceciliazinha <ceciliacandidosilva21@gmail.com>
2019-04-16 14:36:36 -0300

    Criando a classe UIComponets e fazendo as referencias

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -9,6 +9,7 @@ public class SceneManager : MonoBehaviour
     private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f);
     [SerializeField]
     private Transform Tape = null;
+    public UIComponets uiComponets;
     
     void Awake(){
         Assert.IsNotNull(Tape);
